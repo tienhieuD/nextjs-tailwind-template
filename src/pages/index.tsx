ef161718cfaa910c0ce027d87fca9979b80ca6ea
hieudt <hieudt@ftech.ai>
2022-06-27 14:03:16 +0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Next.js Boilerplate Presentation"
+          description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
+        />
+      }
+    >
+      <h1 className="text-2xl font-bold">
+        Boilerplate code for your Nextjs project with Tailwind CSS
+      </h1>
+    </Main>
+  );
+};
+
+export default Index;
---
+++ src/templates/Main.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+
+type IMainProps = {
+  meta: ReactNode;
+  children: ReactNode;
+};
+
+const Main = (props: IMainProps) => (
+  <div className="w-full px-1 text-gray-700 antialiased">
+    {props.meta}
+    <div className="mx-auto">
+      <div className="content py-5 text-xl">{props.children}</div>
+    </div>
+  </div>
+);
+
+export { Main };
